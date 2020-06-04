811f5703acb64123004eeb1a097f5d6da6a28cfc
Artyom Maksimov <temicch@gmail.com>
2020-06-05 02:38:17 +0700

    MahApps and Avalon dock migration

--- AO_AddonMaker/Views/MainWindow.xaml.cs
+++ AO_AddonMaker/Views/MainWindow.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows;
+using Xceed.Wpf.AvalonDock.Themes;
 
 namespace AO_AddonMaker
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            //DockingManager.Theme = new VS2010Theme();
+            
         }
     }
 }
--- AO_AddonMaker/Widget/Widget.cs
+++ AO_AddonMaker/Widget/Widget.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Linq;
 using System.Xml.Serialization;
 
@@ -9,13 +12,25 @@ namespace AO_AddonMaker
     [Serializable]
     abstract public class Widget : AddonFile, IUIElement
     {
+        [Category("Base properties")]
+        [Description("Системное название виджета")]
         public string Name { get; set; }
+        [Category("Base properties")]
+        [Description("Виден ли виджет. По умолчанию true. если виджет не виден, то он недоступен и для реакций")]
         public bool Visible { get; set; }
-        public bool Enabled { get; set; }
+        [Category("Base properties")]
+        [Description("Доступен ли виджет и все его дочерние виджеты для реакций. Может влиять на внешний вид (виджет \"засеривается\"). По умолчанию true")]
+        public bool Enabled { get; set; } = true;
+        [Category("Base properties")]
+        [Description("Приоритет отображения (также влияет на обработку мышиных событий) виджета в списке виджетов своего родителя. То есть с помощью этого поля можно сформировать иерархию отображения виджетов всего аддона")]
         public int Priority { get; set; }
 
+        [Category("Placement")]
+        [Description("Описание расположение виджета")]
         public WidgetPlacementXY Placement { get; set; }
 
+        [Category("Children widgets")]
+        [Description("Дочерние виджеты. Почти каждый виджет может содержать дочерние виджеты, за исключением особых случаев типа слайдера и т.п. Дочерние виджеты отображаются поверх родителя и перехватывают реакции (если они объявлены и на них подписаны обработчики) раньше родительского виджета за исключением особых случаев")]
         [XmlArrayItem("Item")]
         public List<href> Children { get; set; }
 
@@ -25,28 +40,67 @@ namespace AO_AddonMaker
             get => Children.Select(x => x.File).ToList();
         }
 
-        public bool clipContent { get; set; }
+        [Category("Children widgets")]
+        [Description("Нужно ли обрезать содержимое, включая дочерние виджеты, по границам данного. По умолчанию false")]
+        public bool clipContent { get; set; } = false;
+
+        [Category("Display")]
+        [Description("Слой для отображения нижней части текстуры")]
         public href BackLayer { get; set; }
+        [Category("Display")]
+        [Description("Слой для отображения верхней части текстуры")]
         public href FrontLayer { get; set; }
+        [Category("Display")]
+        [Description("Текстура с альфой. Используется для задания маски, по которой будет обрезана основная текстура данного контрола и всех его детей")]
         public href textureMask { get; set; }
-        public float fade { get; set; }
+        [Category("Display")]
+        [Description("Визуальная прозрачность виджета. По умолчанию 1.0f - непрозрачен")]
+        public float fade { get; set; } = 1f;
 
+        [Category("Track and limit reactions")]
+        [Description("Черно-белая текстура (по степеням 2) для задания активной (белые пиксели) области для кликов мышью. Нужно вручную выставлять mipSW = 0 при экспорте")]
         public href pickMask { get; set; }
+        [Category("Track and limit reactions")]
+        [Description("Обрабатывать мышиные реакции только для детей этого виджета, игнорируя сам виджет")]
         public bool PickChildrenOnly { get; set; }
+        [Category("Track and limit reactions")]
+        [Description("Игнорировать PickChildrenOnly при скролировании колесом мыши и наведении. Всегда обрабатывать реакцию скролла колесом мыши")]
         public bool forceWheel { get; set; }
+        [Category("Track and limit reactions")]
+        [Description("Игнорировать двойной клик мышью для виджета и для его детей")]
         public bool IgnoreDblClick { get; set; }
-        public bool TransparentInput { get; set; }
+        [Category("Track and limit reactions")]
+        [Description("Является ли виджет прозрачным для ввода. По умолчанию false")]
+        public bool TransparentInput { get; set; } = false;
 
-        public bool isProtected { get; set; }
+        [Category("Special")]
+        [Description("Запрещать ли пользовательским аддонам операции с виджетом. По умолчанию false")]
+        public bool isProtected { get; set; } = false;
+        [Category("Special")]
+        [Description("Задаёт порядок обхода контролов по клавише Tab. По умолчанию 0(не учавствует в обходе). Для участия в обходе значение должно быть больше 0")]
         public int TabOrder { get; set; }
+        [Category("Special")]
+        [Description("Звук на начатое отображение виджета")]
         public href soundShow { get; set; }
+        [Category("Special")]
+        [Description("Звук на скрытие виджета")]
         public href soundHide { get; set; }
 
+        [Category("Keyboard reactions")]
+        [Description("Список реакций на клавиатурные нажатия")]
         public List<BindSection> bindSections { get; set; }
 
+        [Category("Mouse reactions")]
+        [Description("Уведомление о наведении на виджет. (Кроме виджетов со специальной обработкой - кнопок и т.д.)")]
         public string reactionOnPointing { get; set; }
+        [Category("Mouse reactions")]
+        [Description("Уведомление о наведении на виджет вне зависимости от его доступности для кликов. (Использовать только при сильной необходимости - потребляет много ресурсов.)")]
         public string forceReactionOnPointing { get; set; }
+        [Category("Mouse reactions")]
+        [Description("Уведомление о прокрутке колёсика мыши вверх")]
         public string reactionWheelUp { get; set; }
+        [Category("Mouse reactions")]
+        [Description("Уведомление о прокрутке колёсика мыши вниз")]
         public string reactionWheelDown { get; set; }
 
         public Widget()
