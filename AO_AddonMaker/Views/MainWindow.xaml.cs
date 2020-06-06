7fcc89532634ec0ae8da9f015e059ea458029954
Artyom Maksimov <temicch@gmail.com>
2020-06-07 06:15:06 +0700

    "Fix"

--- AO_AddonMaker/Views/MainWindow.xaml.cs
+++ AO_AddonMaker/Views/MainWindow.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections.ObjectModel;
-using System.Windows;
-using Xceed.Wpf.AvalonDock.Themes;
-
-namespace AO_AddonMaker
+﻿namespace AO_AddonMaker
 {
     public partial class MainWindow : MahApps.Metro.Controls.MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            //DockingManager.Theme = new VS2010Theme();
-            
         }
     }
 }
