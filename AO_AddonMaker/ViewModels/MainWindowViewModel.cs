aaf79fd283a501edd16a7f02540066bd1c7d51a1
Artyom Maksimov <temicch@gmail.com>
2020-06-10 05:32:58 +0700

    Menu redesigned, added sample menu list. "Load project" moved to a separate method

--- AO_AddonMaker/ViewModels/MainWindowViewModel.cs
+++ AO_AddonMaker/ViewModels/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using MahApps.Metro.Controls;
 using Microsoft.Win32;
 using System;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,10 @@ namespace AO_AddonMaker.Views
         public Project Project { get; set; }
 
         public ObservableCollection<IUIElement> RootFile { get; set; }
+        public ObservableCollection<string> Samples { get; private set; }
+
+        private const string samplesPath = "../../../Samples";
+        private const string addonDescName = "AddonDesc.(UIAddon).xdb";
 
         private StringBuilder textDebug = new StringBuilder();
 
@@ -55,39 +60,81 @@ namespace AO_AddonMaker.Views
             }
         }
 
+        RelayCommand _sampleSelect;
+        public ICommand sampleSelect
+        {
+            get
+            {
+                if (_sampleSelect == null)
+                    _sampleSelect = new RelayCommand(SampleSelect);
+                return _sampleSelect;
+            }
+        }
+
         public MainWindowViewModel()
         {
             AO_AddonMaker.DebugOutput.Init(this);
             AddonFileManager.OnDebug += DebugWrite;
             AddonFileManager.Clear();
             RootFile = new ObservableCollection<IUIElement>();
+
+            InitSampleProjects();
         }
 
-        private async void OpenFile(object parameter)
+        private void InitSampleProjects()
+        {
+            Samples = new ObservableCollection<string>();
+            var previousDirectory = Directory.GetCurrentDirectory();
+            try
+            {
+                foreach(var path in Directory.GetDirectories(samplesPath))
+                {
+                    var addonDesc = $"{path}\\{addonDescName}";
+                    if (!File.Exists(addonDesc))
+                        continue;
+                    var q = Directory.GetParent(addonDesc).Name;
+                    Samples.Add(q);
+                }
+            }
+            catch (Exception) { }
+            Directory.SetCurrentDirectory(previousDirectory);
+        }
+
+        private void OpenFile(object parameter)
         {
             OpenFileDialog dlg = new OpenFileDialog
             {
                 DefaultExt = ".xdb",
-                Filter = "AddonDesc|AddonDesc.(UIAddon).xdb|All files|*.*"
+                Filter = $"AddonDesc|{addonDescName}|All files|*.*"
             };
             bool? result = dlg.ShowDialog();
             if (result == true)
             {
-                var workInProgress = new WorkInProgress()
-                {
-                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
-                    Owner = Application.Current.Windows.OfType<MetroWindow>().SingleOrDefault(x => x.IsActive)
-                };
-                workInProgress.Show();
-                RootFile.Clear();
-                await Task.Run(() => LoadProject(dlg.FileName));
-                RootFile.Add(Project.RootWidget);
-                workInProgress.Close();
+                AsyncLoadProject(dlg.FileName);
             }
+        }
+
+        private async void AsyncLoadProject(string filePath)
+        {
+            var workInProgress = new WorkInProgress()
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                Owner = Application.Current.Windows.OfType<MetroWindow>().SingleOrDefault(x => x.IsActive)
+            };
+            workInProgress.Show();
+            RootFile.Clear();
+            await Task.Run(() => LoadProject(filePath));
+            RootFile.Add(Project.RootWidget);
+            workInProgress.Close();
         }
 
         private void LoadProject(string fileName)
         {
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("The specified file does not exist");
+                return;
+            }
             GC.Collect();
             GC.WaitForFullGCComplete();
             Project = new Project(fileName);
@@ -97,13 +144,17 @@ namespace AO_AddonMaker.Views
         public void DebugWrite(string msg)
         {
             DebugOutput = string.Format("{0}\n", msg);
-            //Debug.WriteLine(msg);
         }
 
         private void ClearDebug(object parameter)
         {
             textDebug.Clear();
             OnPropertyChanged(nameof(DebugOutput));
+        }
+
+        private void SampleSelect(object parameter)
+        {
+            AsyncLoadProject($"{samplesPath}\\{parameter}\\{addonDescName}");
         }
     }
 }
