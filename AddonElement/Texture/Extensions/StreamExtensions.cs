f658897bc99191d3900ca486b7a53eb971fe188c
Artyom Maksimov <temicch@gmail.com>
2020-08-15 10:50:01 +0700

    Fix

--- AddonElement/Texture/Extensions/StreamExtensions.cs
+++ AddonElement/Texture/Extensions/StreamExtensions.cs
@@ -13,13 +13,15 @@ namespace Application.BL.Texture.Extensions
         public static MemoryStream UnZLib(this Stream input)
         {
             input.Position = 0L;
+            var inputLength = (int)input.Length;
+
             var memoryStream = new MemoryStream();
             using (var zlibStream = new GZipStream(input, CompressionMode.Decompress))
             {
-                var buffer = new byte[16384];
-                for (var count = zlibStream.Read(buffer, 0, 16384);
+                var buffer = new byte[inputLength];
+                for (var count = zlibStream.Read(buffer, 0, inputLength);
                     count > 0;
-                    count = zlibStream.Read(buffer, 0, 16384))
+                    count = zlibStream.Read(buffer, 0, inputLength))
                     memoryStream.Write(buffer, 0, count);
             }
 
